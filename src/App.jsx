91364c5fbb465959578911d7ff0fe35bb60a234c
RajeshKumarG18 <rajesh.kumar@techpearl.com>
2026-03-25 16:53:45 +0530

    Updated the changes in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const [step, setStep] = useState("");
 const [history, setHistory] = useState([]);
 
 
+
 // Saves previous count history
 const updateCount = (operation) => {
 setHistory(prev => [...prev, count]);
@@ -14,36 +15,52 @@ setCount(prev => operation(prev));
 };
 
 
+
 // Increments the count value enetred by the end user
 const increment = () => {
+
 const value = Number(step) || 0;
+
 updateCount(prev => prev + value);
 };
 
 
+
 // Decrements the count value enetred by the end user
 const decrement = () => {
+
 const value = Number(step) || 0;
+
 updateCount(prev => prev - value);
 };
 
 
+
+// Resets the count value to 0
+const reset = () => {
+
+updateCount( () => 0);
+};
+
+
+
 // Undo the previous count output
 const undo = () => {
-if (history.length === 0) return;
-setCount(history[history.length - 1]);
+
+if (history.length === 0)
+  return;
+
+setCount(history [history.length - 1]);
+
 setHistory(prev => prev.slice(0, -1));
 };
 
 
-const reset = () => {
-updateCount(() => 0);
-};
 
 
 return (
 
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
+    <div style={{ textAlign: "center", marginTop: "100px"}}>
   
       <h1>Counter App</h1>
       <h2>{count}</h2>
@@ -58,12 +75,12 @@ return (
       </div>
 
 
-      <div style={{ display: "flex", justifyContent: "center", gap: "15px" }}>
+      <div style={{ display: "flex", justifyContent: "center", gap: "15px"}}>
 
         <button onClick={increment}>Increase</button>
         <button onClick={decrement}>Decrease</button>
-        <button onClick={undo}>Undo</button>
         <button onClick={reset}>Reset</button>
+        <button onClick={undo}>Undo</button>
 
       </div>
     </div>
